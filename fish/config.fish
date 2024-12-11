93433cc9ed9dfadfa47c2fe0ba294ea0be575bfb
Saifullah Balghari <145737234+Saifullah-Balghari@users.noreply.github.com>
2024-12-12 02:06:12 +0500

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,19 @@
+set -g fish_greeting
+neofetch
+atuin init fish | source
+
+# List Directory
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
