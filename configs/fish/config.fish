769e08d08cccbff0c2c552f3628257b7ca28c20e
Saifullah-Balghari <balgharisaifullah@gmail.com>
2025-02-21 00:44:07 +0500

    Move enviroment variables to hyprland config

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -16,12 +16,3 @@ alias ls='eza -1   --icons=auto' # short list
 alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
 alias ld='eza -lhD --icons=auto' # long list dirs
 alias lt='eza --icons=auto --tree' # list folder as tree
-
-# Variables
-set -Ux GTK_CURSOR_THEME Bibata-Modern-Classic
-set -Ux XCURSOR_THEME Bibata-Modern-Classic
-set -Ux XCURSOR_SIZE 20
-set -x QT_QPA_PLATFORMTHEME qt5ct
-set -x QT_QPA_PLATFORMTHEME qt6ct
-set -x QT_QPA_PLATFORM wayland
-set -x QT_STYLE_OVERRIDE kvantum
