14c0dee95d6dca21ca0c23147725aa92a704d64d
Saifullah-Balghari <balgharisaifullah@gmail.com>
2025-03-14 08:31:45 +0500

    Add functions for backup_files and copy_files

--- lib/shared/library.sh
+++ lib/shared/library.sh
@@ -45,3 +45,41 @@ installPackages() {
     done
     log "Success" "$package_group_name packages installation complete."
 }
+
+# Function to backup existing files
+backupFiles() {
+    local source_dir=$1
+    local backup_dir=$2
+    local files=("${!3}")
+
+    for item in "${items[@]}"; do
+        if [ -e "$source_dir/$item" ]; then
+            mkdir -p "$backup_dir"
+            if [ -e "$backup_dir/$item" ]; then
+                # If the item already exists in the backup directory, remove it first
+                rm -rf "$backup_dir/$item"
+                log "Info" "Removed existing $backup_dir/$item to avoid conflicts."
+            fi
+            mv "$source_dir/$item" "$backup_dir/"
+            log "Info" "Backed up $source_dir/$item to $backup_dir/"
+        else
+            log "Warning" "$source_dir/$item does not exist. Skipping backup."
+        fi
+    done
+}
+
+# Function to copy new files
+copyFiles() {
+    local source_dir=$1
+    local target_dir=$2
+    local files=("${!3}")
+
+    for file in "${files[@]}"; do
+        if [ -e "$source_dir/$file" ]; then
+            cp -r "$source_dir/$file" "$target_dir/"
+            log "Info" "Copied $source_dir/$file to $target_dir/"
+        else
+            log "Warning" "Source file $source_dir/$file does not exist."
+        fi
+    done
+}
